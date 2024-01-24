cf50d16576d9eb15fdeaa3284ed05cb7fe0824ee
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-24 15:26:26 -0300

    feat: config sqlalchemy and create class product

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # inicializa o Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+# Modelagem dos dados
+# Produto(id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
 
 # Definição das rotas
 @app.route('/')
