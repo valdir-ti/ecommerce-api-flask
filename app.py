9f09319ef91e34d0ab23ffa780ed1c2b812c013a
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-01-24 15:46:00 -0300

    feat: add product

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 # inicializa o Flask
@@ -9,18 +9,27 @@ db = SQLAlchemy(app)
 
 # Modelagem dos dados
 # Produto(id, name, price, description)
-
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-
 # Definição das rotas
 @app.route('/')
 def helloWorld():
-    return 'Hello World' 
+    return 'App is running!' 
+
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data['name'],price=data['price'],description=data.get('description', ''))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({'message': 'Produto added successfully'})
+    
+    return jsonify({'message': 'Invalid product data'}), 400
 
 # Executando o app com o método de depuração ativado
 if __name__ == '__main__':
